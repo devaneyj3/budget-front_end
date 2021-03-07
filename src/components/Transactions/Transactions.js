38072f823df35b1b2ddec0086acc2b132eb51576
Jordan <jordandevaney28@gmail.com>
2021-03-07 11:05:54 -0500

    use font awesome library

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,17 +1,43 @@
+import { useState } from "react";
 import { connect } from "react-redux";
-import { Alert } from "reactstrap";
+import { Button } from "reactstrap";
 import moment from "moment";
 import "./Transactions.scss";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Transactions = ({ id, price, description, name, category, created }) => {
+  const [showOptions, setShowOptions] = useState(false);
+  const editTransaction = (transaction) => {
+    console.log("editing, ", transaction);
+  };
+  const deleteTransaction = (transaction) => {
+    console.log("deleting, ", transaction);
+  };
   return (
     <>
-      <tr>
+      <tr
+        onMouseEnter={() => setShowOptions(true)}
+        onMouseLeave={() => setShowOptions(false)}
+      >
         <td>{name}</td>
         <td>{description}</td>
         <td className="price">${price.toLocaleString()}</td>
         <td>{category}</td>
         <td>{moment(created).format("MMMM do YYYY")}</td>
+        {showOptions ? (
+          <>
+            <FontAwesomeIcon
+              icon={faEdit}
+              onClick={() => editTransaction(name)}
+            />
+            <FontAwesomeIcon
+              icon={faTrash}
+              onClick={() => deleteTransaction(name)}
+            />
+          </>
+        ) : null}
       </tr>
     </>
   );
