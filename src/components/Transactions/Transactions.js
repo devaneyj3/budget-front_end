69c759cb01ba5800dc442e5099b6652e12e3bc8d
Jordan <jordandevaney28@gmail.com>
2021-03-20 21:35:16 -0400

    add modal for editing and deleting

--- src/components/Transactions/Transactions.js
+++ src/components/Transactions/Transactions.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import moment from "moment";
 import { deleteTransaction } from "../../redux/actions/transactionActions";
@@ -5,6 +6,14 @@ import "./Transactions.scss";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { faEdit } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  Button,
+  Alert,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "reactstrap";
 
 const Transactions = ({
   id,
@@ -19,36 +28,60 @@ const Transactions = ({
 }) => {
   const editItem = (transaction) => {
     console.log("editing, ", transaction);
+    toggle();
   };
   const deleteItem = (id) => {
     deleteTransaction(id);
+    toggle();
   };
+
+  const [modal, setModal] = useState(false);
+  const [blur, setBlur] = useState(false);
+
+  const toggle = () => setModal(!modal);
   return (
-    <tr>
-      <td>{name}</td>
-      <td>{description}</td>
-      <td>{account}</td>
-      <td className={`price ${type === "inc" ? "inc" : "exp"}`}>
-        ${price.toLocaleString()}
-      </td>
-      <td>{category}</td>
-      <td>{moment(created).format("MMMM do YYYY")}</td>
-      {/* 
-      <td>
-        <FontAwesomeIcon
-          className="icon"
-          icon={faEdit}
-          onClick={() => editItem(name)}
-        />
-      </td>
-      <td>
-        <FontAwesomeIcon
-          className="icon"
-          icon={faTrash}
-          onClick={() => deleteItem(id)}
-        />
-      </td> */}
-    </tr>
+    <>
+      {blur === false ? (
+        <tr onMouseEnter={() => setBlur(true)}>
+          <td>{name}</td>
+          <td>{description}</td>
+          <td>{account}</td>
+          <td className={`price ${type === "inc" ? "inc" : "exp"}`}>
+            ${price.toLocaleString()}
+          </td>
+          <td>{category}</td>
+          <td>{moment(created).format("MMMM do YYYY")}</td>
+        </tr>
+      ) : (
+        <tr onMouseLeave={() => setBlur(false)}>
+          <td colSpan="6">
+            <section className="btn-container">
+              <Button color="info" onClick={toggle}>
+                <FontAwesomeIcon className="icon" icon={faEdit} />
+                Edit
+              </Button>
+              <Button color="danger" onClick={toggle}>
+                <FontAwesomeIcon className="icon" icon={faTrash} />
+                Delete
+              </Button>
+              <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle} style={{ textAlign: "center" }}>
+                  Are you sure you want to delete?
+                </ModalHeader>
+                <ModalFooter>
+                  <Button color="primary" onClick={toggle}>
+                    No
+                  </Button>{" "}
+                  <Button color="success" onClick={() => deleteItem(id)}>
+                    Yes
+                  </Button>
+                </ModalFooter>
+              </Modal>
+            </section>
+          </td>
+        </tr>
+      )}
+    </>
   );
 };
 
